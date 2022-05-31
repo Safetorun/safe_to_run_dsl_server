81b29c4055dcbafdac9d17f79af51f216fbca82d
dllewellyn <danieln.llewellyn@gmail.com>
2022-05-31 08:43:27 +0100

    Removing github files that are unused

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,21 @@
 terraform {
   required_version = "1.1.7"
+
+  cloud {
+    organization = "safetorun"
+    workspaces {
+      name = "safe-to-run-compiler-server"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.8.0"
     }
   }
+}
+
+provider "aws" {
+  region = "eu-west-1"
 }
