d561206aa66583026644db69ce8f47535b85a867
dllewellyn <danieln.llewellyn@gmail.com>
2022-06-01 19:26:36 +0100

    Adding CORS to post

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -29,6 +29,24 @@ resource "aws_api_gateway_method" "options_method" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_method_response" "options_200_post" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.gw_resource.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
 resource "aws_api_gateway_method_response" "options_200" {
   rest_api_id = aws_api_gateway_rest_api.gw.id
   resource_id = aws_api_gateway_resource.gw_resource.id
